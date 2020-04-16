260e6a9a787eac9bfb5fe0037afe403b7e21f57b
zeke <emostov@middlebury.edu>
2020-04-15 18:53:50 -0700

    read in cl args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env; 
+
 fn main() {
-    println!("Hello, world!");
+    // Gather and creat vector cl args pass to program, will not accept invlid unicode
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
