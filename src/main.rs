d87d3a42c94417bb243f0a95cf99c2b3ddb5e203
zeke <emostov@middlebury.edu>
2020-04-15 19:38:57 -0700

    set up config struct and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use std::env; 
+use std::fs;
 
+// Responsible for parsing logic, setting up configuration, calling a 
+// run function in lib.rs, and handling errors from lib.rs
+// main.rs concern is running the program.
 fn main() {
     // Gather and creat vector cl args pass to program, will not accept invlid unicode
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let config = parse_config(&args);
+
+    // Opens file and returns a Result<String> of the contents
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    // Here, clone is not the most efficient means, but it keeps the code simple
+    // and we only do it once so its const is constant
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
